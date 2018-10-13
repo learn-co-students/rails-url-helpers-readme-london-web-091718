44b5537866d8dc23bca634cf481dc9c6d83bf4d3
Ed Putans <edputans@gmail.com>
2018-10-13 19:01:08 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
   resources :posts, only: [:index, :show]
+  
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create
+Post.create(title: "DADSA")
